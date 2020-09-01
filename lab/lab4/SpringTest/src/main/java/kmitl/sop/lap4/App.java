e1bc78ff737f70e9e8f503431aa90879b6d5b986
stupidwolfy <61070268@kmitl.ac.th>
2020-09-01 23:26:54 +0700

    add lab4

--- lab/lab4/SpringTest/src/main/java/kmitl/sop/lap4/App.java
+++ lab/lab4/SpringTest/src/main/java/kmitl/sop/lap4/App.java
@@ -1,8 +1,13 @@
 package kmitl.sop.lap4;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,15 +20,41 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableAutoConfiguration
-public class App 
-{
-    public static void main( String[] args )
-    {
-        SpringApplication.run(App.class, args);
-    }
-    
-    @RequestMapping(value="/", method = RequestMethod.GET)
-    public String home() {
-    	return "Hello World";
-    }
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String hello(@RequestBody Object object) {
+		return String.format("{\"payload\":\"%s\"}", object);
+	}
+
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	public ResponseEntity<Car> get() {
+		Car car = new Car();
+		car.setColor("Blue");
+		car.setType("Truck");
+		car.setDisplacement(1500);
+		car.hasSunroof(false);
+		car.setSpeed(60);
+		return new ResponseEntity<Car>(car, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
+	public ResponseEntity<Car> update(@RequestBody Car car) {
+		String color = car.getColor();
+		String type = car.getType();
+		int displacement = car.getDisplacement();
+		boolean hasSubroof = car.hasSunroof();
+
+		return new ResponseEntity<Car>(car, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/updateMany", method = RequestMethod.POST)
+	public ResponseEntity<List<Car>> updateMany(@RequestBody List<Car> cars){
+		cars.stream().forEach(c -> c.setSpeed(c.getSpeed()+ 10));
+		
+		return new ResponseEntity<List<Car>>(cars, HttpStatus.OK);
+	}
 }
---
+++ lab/lab4/SpringTest/src/main/java/kmitl/sop/lap4/Car.java
@@ -0,0 +1,41 @@
+package kmitl.sop.lap4;
+
+public class Car {
+	private String color, Type;
+	private Integer displacement, speed;
+	private Boolean sunroof;
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getType() {
+		return Type;
+	}
+	public void setType(String type) {
+		Type = type;
+	}
+	public Integer getDisplacement() {
+		return displacement;
+	}
+	public void setDisplacement(Integer displacement) {
+		this.displacement = displacement;
+	}
+	public Integer getSpeed() {
+		return speed;
+	}
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+	public Boolean hasSunroof() {
+		return sunroof;
+	}
+	public void setSunroof(Boolean sunroof) {
+		this.sunroof = sunroof;
+	}
+	
+	
+	
+}
